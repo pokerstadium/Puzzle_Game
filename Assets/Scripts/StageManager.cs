073616816cf5799c39c783cff251b98f2f9ccc58
フカヒレ <86329124+pokerstadium@users.noreply.github.com>
2022-06-12 22:01:17 +0900

    コミット

---
+++ Assets/Scripts/StageManager.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+
+public class StageManager : MonoBehaviour
+{
+    public TextAsset stageFile;
+    private TileType[,] tileTable;
+    public TileManager tilePrefab;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        LoadStageFromText();
+        DebugTable();
+        CreateStage();
+    }
+
+    void CreateStage()
+    {
+        for (int y = 0; y < tileTable.GetLength(1); y++)
+        {
+            for (int x = 0; x < tileTable.GetLength(0); x++)
+            {
+                TileManager tile = Instantiate(tilePrefab);
+                tile.SetType(tileTable[x,y]);
+                Vector2Int position = new Vector2Int(x, y);
+                tile.transform.position = (Vector2)position;
+            }
+        }
+
+    }
+
+    private void LoadStageFromText()
+    {
+        // 空白を区切って改行
+        //  System.StringSplitOptions.RemoveEmptyEntriesは空白を削除する意味でとりあえず入れておけばOK
+        string[] lines = stageFile.text.Split(new[] {'\n','\r'}, System.StringSplitOptions.RemoveEmptyEntries);
+        int columns = 5;
+        int rows = 5;
+        tileTable = new TileType[columns, rows];
+        for (int y = 0; y<rows; y++)
+        {
+            string[] values = lines[y].Split(new[] {','});
+            for (int x = 0; x < columns; x++)
+            {
+                if (values[x] == "0")
+                {
+                    tileTable[x, y] = TileType.DEATH;
+                }
+                if (values[x] == "1")
+                {
+                    tileTable[x, y] = TileType.ALIVE;
+                }
+            }
+        }
+    }
+
+    //デバッグ用
+    void DebugTable()
+    {
+        for (int y = 0; y < 5; y++)
+        {
+            string debugText = "";
+            for (int x = 0; x < 5; x++)
+            {
+                debugText += tileTable[x, y] + ",";
+            }
+            Debug.Log(debugText);
+        }
+    }
+}
---
+++ Assets/Scripts/TileManager.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+
+public enum TileType
+{
+    DEATH,
+    ALIVE
+}
+
+public class TileManager : MonoBehaviour
+{
+    public TileType type;
+    private SpriteRenderer spriteRenderer;
+    public Sprite deathSprite;
+    public Sprite aliveSprite;
+
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+    }
+
+    public void SetType(TileType tileType)
+    {
+        type = tileType;
+        SetImage(type);
+    }
+
+    private void SetImage(TileType type)
+    {
+        if (type == TileType.DEATH)
+        {
+            spriteRenderer.sprite = deathSprite;
+        }
+        else if (type == TileType.ALIVE)
+        {
+            spriteRenderer.sprite = aliveSprite;
+        }
+    }
+
+    public void OnTile()
+    {
+        ReverseTile();
+    }
+
+    private void ReverseTile()
+    {
+        if (type == TileType.DEATH)
+        {
+            SetType(TileType.ALIVE);
+        }
+        else if (type == TileType.ALIVE)
+        {
+            SetType(TileType.DEATH);
+        }
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+    }
+}
