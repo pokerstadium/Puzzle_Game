8e1f5dfa519fca0365005603f2e42d318890ce0e
フカヒレ <86329124+pokerstadium@users.noreply.github.com>
2022-06-25 10:47:22 +0900

    タイトル画面修正

---
+++ Assets/Scripts/Method.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Method : MonoBehaviour
+{
+    public GameObject method;
+
+    public void OnMethod()
+    {
+        method.SetActive(true);
+    }
+
+    public void OnStart()
+    {
+        method.SetActive(false);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
