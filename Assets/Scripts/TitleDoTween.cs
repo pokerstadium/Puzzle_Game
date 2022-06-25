13c2c53501778e6636537a96b93701162212fc22
フカヒレ <86329124+pokerstadium@users.noreply.github.com>
2022-06-25 18:17:40 +0900

    UI作成

---
+++ Assets/Scripts/TitleDoTween.cs
@@ -0,0 +1,68 @@
+﻿using DG.Tweening;
+using UnityEngine;
+using System;
+using System.Collections;
+
+public class TitleDoTween : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Start()
+    {
+
+        if(gameObject.tag == "Fruit1")
+        {
+            Fruit1();
+        }
+        else
+        {
+            Fruit2();
+        }
+    }
+
+    public void Fruit1()
+    {
+        var seq = DOTween.Sequence();
+
+        seq.Append(transform.DOMoveY(-0.5f, 1f).SetEase(Ease.OutSine)).SetRelative();
+
+        seq.Append(transform.DOMoveY(1f, 2f).SetEase(Ease.InOutSine)).SetRelative();
+
+        seq.Append(transform.DOMoveY(-0.5f, 1f).SetEase(Ease.InSine)).SetRelative();
+
+        // 無限ループ
+        seq.SetLoops(-1);
+    }
+
+    public void Fruit2()
+    {
+        var seq = DOTween.Sequence();
+
+        seq.Append(transform.DOMoveY(0.5f, 1f).SetEase(Ease.OutSine)).SetRelative();
+
+        seq.Append(transform.DOMoveY(-1f, 2f).SetEase(Ease.InOutSine)).SetRelative();
+
+        seq.Append(transform.DOMoveY(0.5f, 1f).SetEase(Ease.InSine)).SetRelative();
+
+        // 無限ループ
+        seq.SetLoops(-1);
+    }
+
+    public void OnClick()
+    {
+        StartCoroutine(OnClickFruit());
+    }
+
+    IEnumerator OnClickFruit()
+    {
+        Debug.Log("クリック検知");
+        transform.DOScale(new Vector3(3, 3, 3), 1f);
+        yield return new WaitForSeconds(1f);
+        transform.DOScale(new Vector3(1.5f, 1.5f, 1), 1f);
+    }
+
+
+    // Update is called once per frame
+    private void Update()
+    {
+    }
+}
---
+++ Assets/Scripts/TitleManager.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class TitleManager : MonoBehaviour
+{
+    [SerializeField] private GameObject descriptionPage1;
+    [SerializeField] private GameObject descriptionPage2;
+    [SerializeField] private GameObject descriptionPage3;
+
+    public void OnDescriptionPage1()
+    {
+        descriptionPage1.SetActive(true);
+    }
+
+    public void OnDescriptionPage2()
+    {
+        descriptionPage2.SetActive(true);
+        descriptionPage1.SetActive(false);
+    }
+    public void OnDescriptionPage3()
+    {
+        descriptionPage3.SetActive(true);
+        descriptionPage2.SetActive(false);
+    }
+
+    public void OnTitle()
+    {
+        descriptionPage1.SetActive(false);
+        descriptionPage2.SetActive(false);
+    }
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+    }
+}
